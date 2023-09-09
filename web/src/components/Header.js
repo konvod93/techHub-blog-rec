117037daaa3db0f6a0b3c6b74257de1b9e445083
Konstantin <57582571+konvod93@users.noreply.github.com>
2023-09-09 20:40:08 +0300

    further creation of Navbar

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,11 +1,14 @@
 import { Link } from "gatsby";
-import React from "react";
-import { MdSearch } from 'react-icons/md'
+import React, { useState } from "react";
+import { MdSearch, MdMenu, MdClose } from 'react-icons/md'
 import { menu } from "../constants/menu";
 import HeaderStyles from '../styles/HeaderStyles';
 import Logo from "./Logo";
+import ActionButton from "./buttons/ActionButton";
+import clsx from 'clsx'
 
 function Header() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
   return (
     <HeaderStyles>
       <div className="container">
@@ -13,8 +16,29 @@ function Header() {
           <div className="logo">
             <Logo />
           </div>
-          <div className="nav__wrapper">
+          <div className={clsx("nav__wrapper", isNavOpen && "open")}>
+            <div className="mobileIcon">
+              <div className="searchIcon">
+                <div className="searchIcon__wrapper">
+                  <MdSearch />
+                </div>
+              </div>
+              <ActionButton
+                className="mobileMenuBtn"
+                onClick={() => setIsNavOpen(true)}
+                onKeyDown={() => setIsNavOpen(true)}
+              >
+                <MdMenu />
+              </ActionButton>
+            </div>
             <nav>
+              <ActionButton
+                className="mobileMenuCloseBtn"
+                onClick={() => setIsNavOpen(false)}
+                onKeyDown={() => setIsNavOpen(false)}
+              >
+                <MdClose />
+              </ActionButton>
               <ul>
                 {menu.map((item) => (
                   <li key={item.path}>
