ddda607e01ce90c42e2e9698c5b8ef1c8d8cc311
Konstantin <57582571+konvod93@users.noreply.github.com>
2023-09-13 19:00:07 +0300

    added social icons in footer

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -4,6 +4,7 @@ import { menu } from '../constants/menu';
 import {FooterStyles} from '../styles/FooterStyles';
 import Logo from './Logo';
 import ParagraphText from './typography/ParagraphText';
+import { socialLinks } from '../constants/socialLinks';
 
 function Footer() {
     return (
@@ -14,12 +15,19 @@ function Footer() {
                     Anim exercitation culpa eu consectetur occaecat magna aute eiusmod nulla do do.
                 </ParagraphText>
                 <ul className="footer__menuList">
-                    {menu.map(item => (
+                    {menu.map((item) => (
                         <li key={item.path}>
                             <Link to={item.path}>{item.title}</Link>
                         </li>
                     ))}
                 </ul>
+                <ul className="footer__socialList">
+                    {socialLinks.map((item)=> (
+                        <li key={item.name}>
+                            <a href={item.url}>{item.icon}</a>
+                        </li>
+                    ))}
+                </ul>               
             </div>
         </FooterStyles>
     );
---
+++ web/src/constants/socialLinks.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { FaYoutube, FaInstagram, FaFacebook, FaTwitter } from 'react-icons/fa'
+
+export const socialLinks = [
+    {
+        name: 'youtube',
+        url: 'https://youtube.com',
+        icon: <FaYoutube></FaYoutube>,
+    },
+    {
+        name: 'instagram',
+        url: 'https://instagram.com',
+        icon: <FaInstagram></FaInstagram>,
+    },
+    {
+        name: 'facebook',
+        url: 'https://facebook.com',
+        icon: <FaFacebook></FaFacebook>,
+    },
+    {
+        name: 'twitter',
+        url: 'https://twitter.com',
+        icon: <FaTwitter></FaTwitter>,
+    }
+]
