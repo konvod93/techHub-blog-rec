072358bb02ea077c66b3e9c7dee103396cc9929d
Konstantin <57582571+konvod93@users.noreply.github.com>
2023-09-19 16:29:22 +0300

    created SEO component

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -7,8 +7,10 @@ module.exports = {
     description: `TechHub Blog is a platform for latest technology news and updates.`,
   },
   plugins: [
+    `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`,
   ],
 };
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import SEO from '../components/SEO';
 import HeroSection from '../components/homepage/HeroSection';
 
-const IndexPage = () => <HeroSection />
+const IndexPage = () => (
+  <>
+    <SEO />
+    <HeroSection />
+  </>
+);
 
 
 export default IndexPage;
